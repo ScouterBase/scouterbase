159612fae87866ada045baeaf778bceab80a1064
JonasKoller <jonas.koller@gmx.ch>
2020-05-31 01:32:03 +0200

    FE: Added german locale

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import localeCH from '@angular/common/locales/de-CH';
 import { AppRoutingModule } from './app-routing.module';
 import { AppContainerComponent } from './container/app-container/app-container.component';
 import { CalendarContainerComponent } from './container/calendar-container/calendar-container.component';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {registerLocaleData} from '@angular/common';
+
+registerLocaleData(localeCH);
 
 @NgModule({
   declarations: [
