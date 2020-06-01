5e294bd7c56594928652a01a68e3d507d6a7a7d5
JonasKoller <jonas.koller@gmx.ch>
2020-06-01 11:41:21 +0200

    BE: Removed unnecessary space

--- server/src/main/java/com/github/scouterbase/scouterbase/adapter/rest/AppointmentResource.java
+++ server/src/main/java/com/github/scouterbase/scouterbase/adapter/rest/AppointmentResource.java
@@ -2,14 +2,9 @@ package com.github.scouterbase.scouterbase.adapter.rest;
 
 import com.github.scouterbase.scouterbase.domain.Appointment;
 import com.github.scouterbase.scouterbase.domain.AppointmentRepository;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/appointments")
@@ -17,34 +12,33 @@ public class AppointmentResource {
 
   private final AppointmentRepository appointmentRepository;
 
-  public AppointmentResource(AppointmentRepository appointmentRepository){
+  public AppointmentResource(final AppointmentRepository appointmentRepository){
     this.appointmentRepository = appointmentRepository;
   }
 
   @GetMapping
-
   public List<Appointment> getAllAppointments(){
-    return appointmentRepository.findAll();
+    return this.appointmentRepository.findAll();
   }
 
   @GetMapping("/{id}")
-  public Appointment getAppointment(@PathVariable long id){
-    return appointmentRepository.findById(id).orElseThrow();
+  public Appointment getAppointment(@PathVariable final long id){
+    return this.appointmentRepository.findById(id).orElseThrow();
   }
 
   @PostMapping
-  public Appointment createAppointment(Appointment appointment) {
-     return appointmentRepository.save(appointment);
+  public Appointment createAppointment(final Appointment appointment) {
+     return this.appointmentRepository.save(appointment);
   }
 
   @PutMapping
-  public Appointment updateAppointment(Appointment appointment) {
-    return appointmentRepository.save(appointment);
+  public Appointment updateAppointment(final Appointment appointment) {
+    return this.appointmentRepository.save(appointment);
   }
 
   @DeleteMapping("/{id}")
-  public void deleteAppointment(@PathVariable long id){
-    appointmentRepository.delete(appointmentRepository.findById(id).orElseThrow());
+  public void deleteAppointment(@PathVariable final long id){
+    this.appointmentRepository.delete(this.appointmentRepository.findById(id).orElseThrow());
   }
 
 }
