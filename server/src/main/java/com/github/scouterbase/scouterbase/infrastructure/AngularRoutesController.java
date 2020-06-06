5857bac2061332a553b25dd22801db6440df6400
Nicolas Schmid <nicolas.schmid@bison-group.com>
2020-06-06 11:49:12 +0200

    redirect angular backend routes to index.html

---
+++ server/src/main/java/com/github/scouterbase/scouterbase/infrastructure/AngularRoutesController.java
@@ -0,0 +1,16 @@
+package com.github.scouterbase.scouterbase.infrastructure;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping({"/*","/**"})
+public class AngularRoutesController {
+
+  @GetMapping()
+  public String redirectToIndex(){
+    return "forward:/index.html";
+  }
+
+}
